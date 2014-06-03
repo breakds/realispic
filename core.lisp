d843e8d73cd12980ca666aad429f9be67a84d39a
Break Yang <BreakDS@gmail.com>
2014-06-03 07:44:10 -0400

    jsx syntax enabled

--- core.lisp
+++ core.lisp
@@ -3,8 +3,65 @@
 
 (in-package #:breakds.realispic)
 
-(defun reason-jsx (body)
-  
 
-(defpsmacro jsx (&body body)
-  
+(declaim (inline regular-symbol))
+(defun regular-symbol (keyword-symbol)
+  (symb (mkstr keyword-symbol)))
+
+(defvar *dom-tags* '(:html :body :head :div
+                     :form :ul :li :input
+                     :button :a))
+
+(defun jsx-rewrite (jsx-expr)
+  (labels ((transform-attribute (attribute)
+             (case (car attribute)
+               (t attribute))))
+    (if (atom jsx-expr) jsx-expr
+        `(,(cond ((member (car jsx-expr) *dom-tags*)
+                  `(@ *react 
+                      *dom* 
+                      ,(regular-symbol (car jsx-expr))))
+                 (t (car jsx-expr)))
+           (create ,@(mapcan #'transform-attribute 
+                             (second jsx-expr)))
+           ,@(mapcar #'jsx-rewrite (cddr jsx-expr))))))
+
+(defun jsx-reader (stream sub-char-a sub-char-b)
+  (declare (ignorable sub-char-a sub-char-b))
+  (if (and (eq (read-char stream t nil t) #\s)
+           (eq (read-char stream t nil t) #\x))
+      (jsx-rewrite (read stream t nil t))
+      (error "Unrecognized jsx trigger. Use #jsx(...).")))
+      
+(defvar *readtable-stack* nil)  
+
+(defun enable-jsx-reader ()
+  (push *readtable* *readtable-stack*)
+  (set-dispatch-macro-character #\# #\j #'jsx-reader))
+
+(defun disable-jsx-reader ()
+  (setq *readtable* (pop *readtable-stack*)))
+
+(defun get-initial-state (states)
+  `(lambda ()
+     (create ,@(loop for state in states
+                  append state))))
+
+(defun process-members (members)
+  (labels ((transform-member (member)
+             (case (car member)
+               (t (cons (regular-symbol (car member))
+                        (cdr member))))))
+    (mapcan #'transform-member members)))
+
+
+(defmacro def-widget (name (&rest states) (&rest members) &body body)
+  `(defun ,name ()
+     `(defvar ,',name ((@ *react create-class)
+                       (create get-initial-state ,',(get-initial-state states)
+                               ,@',(process-members members)
+                               render (lambda ()
+                                        ,@',body))))))
+                                    
+
+
--- package.lisp
+++ package.lisp
@@ -3,5 +3,9 @@
 (defpackage #:breakds.realispic
   (:nicknames #:realispic)
   (:use #:cl
-	#:parenscript))
+	#:parenscript)
+  (:import-from #:swiss-knife
+                #:mkstr
+                #:with-gensyms
+                #:symb))
 
